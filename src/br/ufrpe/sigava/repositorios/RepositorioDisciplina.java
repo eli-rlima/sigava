033430a84028ce906beb249ec51772b0ae2edfa9
elivelton-pc <elivelton.rodrigues@ufrpe.br>
2018-05-29 19:40:36 -0300

    RepositorioDisciplina

--- src/br/ufrpe/sigava/repositorios/RepositorioDisciplina.java
+++ src/br/ufrpe/sigava/repositorios/RepositorioDisciplina.java
@@ -1,4 +1,30 @@
 package br.ufrpe.sigava.repositorios;
 
+import br.ufrpe.sigava.classes.Disciplina;
+
+import java.util.ArrayList;
+
 public class RepositorioDisciplina {
+    private ArrayList<Disciplina> repositorioDisciplina = new ArrayList<Disciplina>();
+
+    public RepositorioDisciplina(){}
+
+    public boolean adicionar(Disciplina disciplina){
+        return this.repositorioDisciplina.add(disciplina);
+    }
+    public boolean remover(Disciplina disciplina){
+        return this.repositorioDisciplina.remove(disciplina);
+    }
+    public Disciplina buscar(String nome){
+        Disciplina disciplina = null;
+        for (int i = 0; i < this.repositorioDisciplina.size(); i++){
+            if (this.repositorioDisciplina.get(i).getNome().equalsIgnoreCase(nome)){
+                disciplina = this.repositorioDisciplina.get(i);
+            }
+        }
+        return disciplina;
+    }
+    public boolean existe(Disciplina disciplina){
+        return this.repositorioDisciplina.contains(disciplina);
+    }
 }
