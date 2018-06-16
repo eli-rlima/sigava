869e165c6e3f7ae62026d6ad8975dffee7b962ec
Unknown <heltonvasconcelos88@gmail.com>
2018-06-16 14:45:41 -0300

    Repositório Tarefa
    Adicionar repositório tarefa.

--- src/br/ufrpe/sigava/repositorios/RepositorioTarefa.java
+++ src/br/ufrpe/sigava/repositorios/RepositorioTarefa.java
@@ -1,4 +1,30 @@
 package br.ufrpe.sigava.repositorios;
+import br.ufrpe.sigava.classes.Tarefa;
+import java.util.ArrayList;
+
 
 public class RepositorioTarefa {
+
+    private ArrayList <Tarefa> repositorioTarefas = new ArrayList<Tarefa>();
+
+
+    public boolean adicionar(Tarefa tarefa){
+        return this.repositorioTarefas.add(tarefa);
+    }
+    public boolean remover(Tarefa tarefa){
+        return this.repositorioTarefas.remove(tarefa);
+    }
+    public Tarefa buscar(int codigo){
+        Tarefa tarefa = null;
+        for (int i = 0; i < this.repositorioTarefas.size(); i++){
+            if (this.repositorioTarefas.get(i).getCodigoTarefa() == codigo){
+                tarefa = this.repositorioTarefas.get(i);
+            }
+        }
+        return tarefa;
+    }
+    public boolean existe(Tarefa tarefa){
+        return this.repositorioTarefas.contains(tarefa);
+    }
 }
+
