c98e3cabb5b057e8e1be86fa2b675fbb0ed24c64
elivelton-pc <elivelton.rodrigues@ufrpe.br>
2018-06-22 01:31:20 -0300

    Atualização do Sistema
    Adição de cadastro e descadastro de disciplina

--- src/br/ufrpe/sigava/gui/Sistema.java
+++ src/br/ufrpe/sigava/gui/Sistema.java
@@ -5,6 +5,7 @@ import br.ufrpe.sigava.negocio.beans.pessoa.Aluno;
 import br.ufrpe.sigava.negocio.beans.pessoa.Professor;
 import br.ufrpe.sigava.negocio.beans.Disciplina;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -13,32 +14,27 @@ public class Sistema {
     private static void MenuAcesso(){
         System.out.println("BEM-VINDO AO SIGAVA\n");
         System.out.println("ESCOLHA UMA OPÇÃO: ");
-        System.out.println("1\t - Cadastrar");
-        System.out.println("2\t - Descadastrar");
-        System.out.println("3\t - Procurar");
-        System.out.println("4\t - Atualizar");
-        System.out.println("5\t - Cadastrar Aluno");
-        System.out.println("6\t - Cadastrar Professor");
-        System.out.println("7\t - Cadastrar Disciplina");
-        System.out.println("8\t - Cadastrar Tarefa");
-        System.out.println("9\t - Descadastrar Aluno");
-        System.out.println("10\t - Descadastrar Professor");
-        System.out.println("11\t - Descadastrar Disciplina");
-        System.out.println("12\t - Descadastrar Tarefa");
-        System.out.println("13\t - Procurar Aluno");
-        System.out.println("14\t - Procurar Professor");
-        System.out.println("15\t - Procurar Disciplina");
-        System.out.println("16\t - Procurar Tarefa");
-        System.out.println("17\t - Listar Alunos");
-        System.out.println("18\t - Listar Professores");
-        System.out.println("19\t - Listar Disciplinas");
-        System.out.println("20\t - Listar Tarefas");
-        System.out.println("21\t - SAIR");
-
+        System.out.println("1\t - Cadastrar Aluno");
+        System.out.println("2\t - Cadastrar Professor");
+        System.out.println("3\t - Cadastrar Disciplina");
+        System.out.println("4\t - Cadastrar Tarefa");
+        System.out.println("5\t - Descadastrar Aluno");
+        System.out.println("6\t - Descadastrar Professor");
+        System.out.println("7\t - Descadastrar Disciplina");
+        System.out.println("8\t - Descadastrar Tarefa");
+        System.out.println("9\t - Procurar Aluno");  //TODO
+        System.out.println("10\t - Procurar Professor"); //TODO
+        System.out.println("11\t - Procurar Disciplina"); //TODO
+        System.out.println("12\t - Procurar Tarefa"); //TODO
+        System.out.println("13\t - Listar Alunos"); //TODO
+        System.out.println("14\t - Listar Professores"); //TODO
+        System.out.println("15\t - Listar Disciplinas"); //TODO
+        System.out.println("16\t - Listar Tarefas"); //TODO
+        System.out.println("17\t - SAIR");
     }
 
-
-    private static Professor cadastroProfessor(Scanner in){
+    private static void cadastroProfessor(IServidorSigava servidorSigava){
+        Scanner in = new Scanner(System.in);
         Professor professor;
         String nome, email, senha, cpf, data;
         String dataR[];
@@ -70,16 +66,18 @@ public class Sistema {
         in.nextLine();
 
         professor = new Professor(nome, email, sexo, dataNascimento, senha, cpf);
-        return professor;
+        servidorSigava.cadastrarProfessor(professor);
     }
 
-    private static void descadastrarProfessor(Scanner in, IServidorSigava servidorSigava){
+    private static void descadastrarProfessor(IServidorSigava servidorSigava){
+        Scanner in = new Scanner(System.in);
         Professor professor;
         String cpf;
         boolean ver;
         System.out.println("CPF do professor: ");
         cpf = in.nextLine();
         professor = servidorSigava.buscarProfessor(cpf);
+
         if (professor == null){
             System.out.println("Professor não cadastrado!");
         }
@@ -91,6 +89,56 @@ public class Sistema {
         }
     }
 
+    private static void cadastrarDisciplina(IServidorSigava servidorSigava){
+        Scanner in = new Scanner(System.in);
+        Disciplina disciplina = null;
+        String nome, data;
+        String dataR[];
+        LocalDate dataInicio;
+        DayOfWeek diaAula;
+        int duracaoAula, cargaHoraria;
+
+        System.out.println("Nome: ");
+        nome = in.nextLine();
+        in.nextLine();
+
+        System.out.println("Data de Início (dd/MM/aaaa): ");
+        data = in.nextLine();
+        dataR = data.split("/");
+        dataInicio = LocalDate.parse(dataR[0] + "-" + dataR[1] + "-" + dataR[2]);
+        diaAula = dataInicio.getDayOfWeek();
+
+        System.out.println("Duração da aula: ");
+        duracaoAula = in.nextInt();
+
+        System.out.println("Carga Horária da disciplina: ");
+        cargaHoraria = in.nextInt();
+
+        disciplina = new Disciplina(nome, dataInicio, diaAula, duracaoAula, cargaHoraria);
+        servidorSigava.cadastrarDisciplina(disciplina);
+    }
+
+    public static void descadastrarDisciplina(IServidorSigava servidorSigava){
+        Scanner in = new Scanner(System.in);
+        Disciplina disciplina;
+        String nome;
+        boolean ver;
+
+        System.out.println("Nome da disciplina: ");
+        nome = in.nextLine();
+        disciplina = servidorSigava.buscarDisciplina(nome);
+
+        if (disciplina == null){
+            System.out.println("Disciplina não cadastrada!");
+        }
+        else {
+            ver = servidorSigava.descadastrarDisciplina(disciplina);
+            if (ver){
+                System.out.println("Descadastrada com sucesso!");
+            }else System.out.println("Não foi possível descadastrar!");
+        }
+    }
+
     public static void Sistema(IServidorSigava servidorSigava){
         Scanner in = new Scanner(System.in);
         int controleMenu = in.nextInt();
@@ -98,6 +146,6 @@ public class Sistema {
 
         do {
 
-        }while (controleMenu != 21);
+        }while (controleMenu != 17);
     }
 }
